a2bed45f0de36e98233ae9eac22faf955832de5c
Alex Yegupov <yegupov@gmail.com>
2024-04-12 09:35:43 +0300

    added all

---
+++ README.md
@@ -0,0 +1,10 @@
+project to test @wttsolutions/eslint-config usage
+
+
+# self-creation
+
+```
+yarn link ../eslint-config
+yarn add @wttsolutions/eslint-config@*
+
+```
---
+++ test/test-react.jsx
@@ -0,0 +1,11 @@
+// emit react/jsx-no-comment-textnodes error
+const component1 = (
+  <div>// incorrect comment</div>
+);
+
+// emit react/no-unknown-property
+const component2 = (
+  <div unknown="property"></div>
+);
+
+console.log(component1, component2);  // eslint-disable-line
